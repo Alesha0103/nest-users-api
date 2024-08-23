892dfa133b9de02b01dcae6302d7ff7d955340fd
Oleksii Skrebtsov <alex.skrebtsov1213@gmail.com>
2024-08-23 13:49:19 +0300

    added .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import { UsersModule } from "./users/module/users.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { DataSource } from "typeorm";
 import { Users } from "./users/enteties/user.entity";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'myusers',
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Users],
       synchronize: true,
       autoLoadEntities: true,
